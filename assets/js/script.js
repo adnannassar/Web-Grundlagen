67c2456b34a9052d4f189d65bd20f4c1a1573b65
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2022-05-24 12:04:02 +0200

    added: last styling css

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var array = [4][3];
+array[0][0] =  'Flug';
+array[0][1] =  'Airline';
+array[0][2] =  'Ziel';
+
+
+array[1][0] =  '100';
+array[1][1] =  'Lufthansa';
+array[1][2] =  'Frakfurt';
+
+
+array[2][0] =  '101';
+array[2][1] =  'Condor';
+array[2][2] =  'Japan';
+
+
+array[3][0] =  '102';
+array[3][1] =  'SunExpress';
+array[3][2] =  'Antalya';
